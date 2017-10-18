ac65e1667d96fd9074c754f8c93a43b695fd409e
djpark <djpark@benple.com>
2017-10-18 17:56:26 +0900

    Revert "1.0.2 CRUD 테스트"
    This reverts commit 8ca51efb67d3204b8cd40456f7b60d25d1f52d0d.

--- src/main/java/com/benple/bbs/board/mapper/BoardMapper.java
+++ src/main/java/com/benple/bbs/board/mapper/BoardMapper.java
@@ -1,32 +1,9 @@
 package com.benple.bbs.board.mapper;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
-
-import com.benple.bbs.board.domain.BoardVO;
 
 @Repository("com.benple.bbs.board.mapper.BoardMapper")
 public interface BoardMapper {
 
-	//게시글 개수
 	public int boardCount() throws Exception;
-
-	//게시글 목록
-	public List<BoardVO> boardList() throws Exception;
-	
-	//게시글 상세
-	public BoardVO boardDetail(int board_seq) throws Exception;
-	
-	//게시글 작성
-	public int boardInsert(BoardVO board);
-	
-	//게시글 수정 
-	public void boardUpdate(BoardVO board) throws Exception;
-	
-	//게시글 삭제
-	public void boardDelete(int board_seq) throws Exception;
-
-
 }
-
